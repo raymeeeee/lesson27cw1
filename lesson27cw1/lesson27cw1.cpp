2b7a1020af19da3f4dec4681dc2c9d9af9429170
raymeeeee <daniapoluektov@gmail.com>
2024-09-30 16:44:42 +0100

    create use in main

--- lesson27cw1/lesson27cw1.cpp
+++ lesson27cw1/lesson27cw1.cpp
@@ -1,6 +1,9 @@
-#include "Product.h"
+#include "Buy.h"
 int main()
 {
-    Product a;
+    Product a("Apple", 100, 0.5);
     a.showProduct();
+    cout << "==================" << endl;
+    Buy b(a, 2);
+    b.printBuy();
 }
